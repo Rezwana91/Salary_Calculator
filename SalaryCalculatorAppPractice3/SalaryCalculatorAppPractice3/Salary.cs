9a8c0f51a4f79967a5da7c1e0c5ac816a8631eba
Rezwana91 <rrananna@gmail.com>
2019-10-15 09:02:49 +0600

    added

---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/Salary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+   public class Salary
+   {
+       public string employeeName;
+       public double totalSalary;
+
+        public string Calculate(double basicAmount, double houseRent, double medical)
+        {
+            totalSalary = basicAmount + (basicAmount / houseRent) + (basicAmount / medical);
+            string message = employeeName + " " + "your salary is" + " " + totalSalary;
+            return message;
+
+        }
+
+    }
+
+   
+}
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        Salary salary = new Salary();
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            //validation check
+
+            if (String.IsNullOrEmpty(employeeNameTextBox.Text))
+            {
+                MessageBox.Show("Employee Name cannnot be empty!");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(basicAmountTextBox.Text))
+            {
+                MessageBox.Show("Basic amount cannnot be empty!");
+                return; 
+            }
+
+            if (String.IsNullOrEmpty(homeRentTextBox.Text))
+            {
+                MessageBox.Show("Home rent cannnot be empty!");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(medicalAllowanceTextBox.Text))
+            {
+                MessageBox.Show("Medical allowance cannnot be empty!");
+                return;
+            }
+
+           double basicAmount = Convert.ToDouble(basicAmountTextBox.Text);
+           double houseRent = Convert.ToDouble(homeRentTextBox.Text);
+           double medical = Convert.ToDouble(medicalAllowanceTextBox.Text);
+
+            salary.employeeName = employeeNameTextBox.Text;
+            MessageBox.Show(salary.Calculate(basicAmount, houseRent, medical));
+
+
+
+
+        }
+    }
+}
